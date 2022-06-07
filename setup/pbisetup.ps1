c2a734805ed1ae5e766a0c2e178de8091186c121
Jen Beiser MSFT <jen.etzler@gmail.com>
2022-06-07 14:38:33 -0700

    Update pbisetup.ps1

--- setup/pbisetup.ps1
+++ setup/pbisetup.ps1
@@ -8,22 +8,25 @@ Install-Module -Name MicrosoftPowerBIMgmt.Workspaces
 Connect-MsolService
 Connect-PowerBIServiceAccount   # or use aliases: Login-PowerBIServiceAccount, Login-PowerBI
 
-Get-PowerBIWorkspace -Scope Organization -All
-
+Get-PowerBIWorkspace -Scope Organization -All | where {$_.Name -like "hacker*workspace" -and $_.state -eq "active"}
+ 
 # Choose capacity to be either embedded or Premium per User from above
 Get-PowerBICapacity -Scope Organization | where {$_.DisplayName -eq "Premium Per User - Reserved"} | foreach {$CapacityId= $_.Id}
 
 # Create hacker workspaces, assign capacity, and add user as contributor
 $userArray = Get-MsolUser -All  | where {$_.DisplayName -like "hacker*" -and $_.isLicensed -eq $true} 
 for ($i=0; $i -lt $userArray.Count; $i++)
-{ 
+{     
     $workspaceName = "$($userArray[$i].DisplayName) workspace"  
+    "Creating workspace " + $workspaceName
     New-PowerBIWorkspace -Name $workspaceName
-
-    $workspace = Get-PowerBIWorkspace -Scope Organization -All | where {$_.Name -eq $workspaceName -and $_.state -eq "active"}    
-    Set-PowerBIWorkspace -Id $workspace.Id -CapacityId $capacityId
     
-    Add-PowerBIWorkspaceUser -Id $workspace.Id -UserEmailAddress $userArray[$i].UserPrincipalName -AccessRight Contributor    
+    $workspace = Get-PowerBIWorkspace -Scope Organization -All | where {$_.Name -eq $workspaceName -and $_.state -eq "active"}    
+    "Adding " + $workspaceName + " to capacity " + $capacityId
+    Set-PowerBIWorkspace -Scope Organization -Id $workspace.Id -CapacityId $capacityId
+    
+    "Adding user " + $userArray[$i].UserPrincipalName + " to " + $workspace.Name 
+    Add-PowerBIWorkspaceUser -Scope Organization -Id $workspace.Id -UserEmailAddress $userArray[$i].UserPrincipalName -AccessRight Contributor    
 }      
 
 # RESET Delete hacker workspaces 
