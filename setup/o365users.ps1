4ce7e3301d0635ac21d71c4ac4c2ca11bd3f7ea2
Jen Beiser MSFT <jen.etzler@gmail.com>
2022-06-07 10:24:56 -0700

    Update o365users.ps1

--- setup/o365users.ps1
+++ setup/o365users.ps1
@@ -1,16 +1,32 @@
-﻿# Connect to Microsoft 365 with PowerShell
+# Connect to Microsoft 365 with PowerShell
 # https://docs.microsoft.com/en-us/microsoft-365/enterprise/connect-to-microsoft-365-powershell?view=o365-worldwide#connect-with-the-azure-active-directory-powershell-for-graph-module
 
 Install-Module AzureAD
 Import-Module  AzureAD
-Connect-AzureAD
 Install-Module MSOnline
-Connect-MsolService
 Install-Module PowerShellGet -Force
 Install-Module Microsoft.Graph -Scope AllUsers
 
+Connect-MsolService
+Connect-AzureAD
+
 Set-ExecutionPolicy RemoteSigned
 Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All
+
+# get the company prefix for the license packages
+$licenseList=Get-MsolAccountSku
+if(($licenseList.GetType().Name) -eq "AccountSkuDetails") 
+{
+    $licensePrefix =$licenseList.AccountSkuId.Split(":")[0]
+}
+
+else
+{
+    $licensePrefix =$licenseList[0].AccountSkuId.Split(":")[0]
+} 
+$tenantName = $licensePrefix
+$licensePackage = $licensePrefix + ":ENTERPRISEPREMIUM"
+
 
 # Remove Office 365 E5 licenses from environment's pre-made user accounts
 # https://docs.microsoft.com/en-us/microsoft-365/enterprise/remove-licenses-from-user-accounts-with-microsoft-365-powershell?view=o365-worldwide
@@ -20,9 +36,8 @@ for ($i=0; $i -lt $userArray.Count; $i++)
 
     $SKUs = @($userArray[$i].Licenses) 
     foreach ($SKU in $SKUs) 
-    {     
-        
-        if ($SKU.AccountSkuId -ieq "M365x25073779:ENTERPRISEPREMIUM") 
+    {             
+        if ($SKU.AccountSkuId -ieq ($licensePrefix + ":ENTERPRISEPREMIUM")) 
         {
             Write "Removing license $($Sku.AccountSkuId) from user $($userArray[$i].UserPrincipalName)"
             Set-MsolUserLicense -UserPrincipalName $userArray[$i].UserPrincipalName -RemoveLicenses $SKU.AccountSkuId
@@ -32,15 +47,25 @@ for ($i=0; $i -lt $userArray.Count; $i++)
 
 # Create Microsoft 365 user accounts for the hackers from csv (UserPrincipalName, FirstName, LastName, DisplayName, UsageLocation, AccountSkuID)
 # https://docs.microsoft.com/en-us/microsoft-365/enterprise/create-user-accounts-with-microsoft-365-powershell?view=o365-worldwide
-Import-Csv -Path "C:\Users\jeetzler\Documents\Users.csv" |
-foreach {New-MsolUser -DisplayName $_.DisplayName -FirstName $_.FirstName -LastName $_.LastName -UserPrincipalName $_.UserPrincipalName -UsageLocation $_.UsageLocation -LicenseAssignment $_.AccountSkuId -ForceChangePassword 0} |
-    Export-Csv -Path "C:\Users\jeetzler\Documents\NewAccounts.csv"
- 
+
+# delete output file with the account info if it exists
+$UserFileName = "C:\Users\jeetzler\Documents\NewAccounts.csv"
+if (Test-Path $UserFileName) 
+{
+  Remove-Item $UserFileName
+}
+
+# create hacker accounts with number suffix i 
+for ($i=16; $i -lt 31; $i++) 
+{    
+    New-MsolUser -DisplayName "hacker$($i)" -UserPrincipalName "hacker$($i)@$($tenantName).onmicrosoft.com" -UsageLocation "US" -LicenseAssignment $licensePackage -ForceChangePassword 0 |
+    Export-Csv -Path "C:\Users\jeetzler\Documents\NewAccounts.csv" -Append
+} 
  
 # RESET Delete hacker user accounts
 # https://docs.microsoft.com/en-us/microsoft-365/enterprise/delete-and-restore-user-accounts-with-microsoft-365-powershell?view=o365-worldwide
 $userArray = Get-MsolUser -All  | where {$_.DisplayName -like "hacker*" -and $_.isLicensed -eq $true} 
 for ($i=0; $i -lt $userArray.Count; $i++)
 {
-    Remove-MsolUser -UserPrincipalName $userArray[$i].UserPrincipalName
+    Remove-MsolUser -UserPrincipalName $userArray[$i].UserPrincipalName -force 
 }
