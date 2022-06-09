e5c8c89fce8c896ce72821a83531e0d3a24ca0b3
Jen Beiser MSFT <jen.etzler@gmail.com>
2022-06-09 08:06:43 -0700

    Add files via upload

---
+++ setup/OneDriveSetup.ps1
@@ -0,0 +1,36 @@
+﻿$Credential = Get-Credential
+Connect-MsolService -Credential $Credential
+Connect-SPOService -Url https://CRM598570-admin.sharepoint.com -Credential $Credential 
+$list = @()
+#Counters
+$i = 0
+
+
+#Get licensed users
+$users = Get-MsolUser -All | Where-Object { $_.islicensed -eq $true -and $_.DisplayName -like "hacker*"}
+#total licensed users
+$count = $users.count
+
+foreach ($u in $users) {
+    $i++
+    Write-Host "$i/$count"
+
+    $upn = $u.userprincipalname
+    $list += $upn
+
+    if ($i -eq 199) {
+        #We reached the limit
+        Request-SPOPersonalSite -UserEmails $list -NoWait
+        Start-Sleep -Milliseconds 655
+        $list = @()
+        $i = 0
+    }
+}
+
+if ($i -gt 0) {
+    Request-SPOPersonalSite -UserEmails $list -NoWait
+}
+
+#Verify
+Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select -ExpandProperty Url  
+ 
